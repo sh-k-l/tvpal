9b67bab1ef86b9e7a6d7acde87c03859b381aa3b
Shakil <shakil.chowdhury@yahoo.com>
2020-05-17 11:42:45 +0100

    fiddle

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/users', users);
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/app/static', express.static(path.join(__dirname, 'client/build')));
+  app.use('/app/static', express.static(path.join(__dirname, 'client/build//static')));
   app.get('/app', (req, res) => {
     console.log('sending index file');
     // res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
