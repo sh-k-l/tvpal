d6e18dca856d8e1a7e34c09e09c53df5c4cb4a63
Shakil <shakil.chowdhury@yahoo.com>
2020-08-24 17:42:50 +0100

    only use morgan in development environment

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,4 @@
 const { json, urlencoded } = require('express');
-const morgan = require('morgan');
 const helmet = require('helmet');
 const passport = require('../config/passport').passport;
 const { NODE_ENV } = require('../constants');
@@ -9,7 +8,7 @@ const setupMiddleware = (app) => {
   app.use(urlencoded({ extended: true }));
   app.use(helmet());
   if (NODE_ENV == 'development') {
-    app.use(morgan('tiny'));
+    app.use(require('morgan')('tiny'));
   }
   app.use(passport.initialize());
 };
